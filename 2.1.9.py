9d8be2157f1b17887734f127bdd545e069688cda
appgenie <16236565+appgenie-p@users.noreply.github.com>
2022-08-20 12:26:31 +0000

    2.1.9 done

--- 2.1.9.py
+++ 2.1.9.py
@@ -1,57 +1,57 @@
-from tkinter.messagebox import NO
+from typing import List, Optional
 
 
 class LinkedList:
     def __init__(self) -> None:
         self.head: 'ObjList' = None
-        # ссылка на первый объект связного списка (если список пустой, то head = None)
         self.tail: 'ObjList' = None
-        # ссылка на последний объект связного списка (если список пустой, то tail = None).
 
     def add_obj(self, obj: 'ObjList') -> None:
         """добавление нового объекта obj класса ObjList в конец связного
         списка
         """
-        # вставка, если есть предыдущее значение
-        if self.head is None:
+        if self.tail:
+            self.tail.set_next(obj)
+        obj.set_prev(self.tail)
+        self.tail = obj
+        if not self.head:
             self.head = obj
-            self.tail = obj
-            self.tail.set_prev(self.head)
-        else:
-            prev_obj = self.tail.get_prev()
-            obj.set_prev(prev_obj)
-            prev_obj.set_next(obj)
-            self.tail = obj
 
-    def remove_obj(self):
-        """удаление последнего объекта из связного списка
-        Взять последний эллемент, проверить свойство __prev
-        if __prev not Null: self.tail = __prev
-        """
-        pass
+    def remove_obj(self) -> None:
+        try:
+            prev = self.tail.get_prev()
+            prev.set_next(None)
+            self.tail = prev
+        except AttributeError:
+            self.head = None
+            self.tail = None
 
-    def get_data(self):
+    def get_data(self) -> List[str]:
         """получение списка из строк локального свойства __data всех объектов
         связного списка
         """
-        pass
+        try:
+            list = []
+            list.append(self.head.get_data())
+            next_obj = self.head.get_next()
+            while next_obj is not None:
+                list.append(next_obj.get_data())
+                next_obj = next_obj.get_next()
+            return list
+        except AttributeError:
+            return []
 
 
 class ObjList:
     def __init__(self, data: str) -> None:
         self.__next: ObjList = None
-        # ссылка на следующий объект связного списка (если следующего объекта нет, то __next = None);
         self.__prev: ObjList = None
-        # ссылка на предыдущий объект связного списка (если предыдущего объекта нет, то __prev = None);
         self.__data: str = data
-        # строка с данными.
 
     def set_next(self, obj: 'ObjList') -> None:
-        #  - изменение приватного свойства __next на значение obj;
         self.__next = obj
 
     def set_prev(self, obj: 'ObjList') -> None:
-        #  - изменение приватного свойства __prev на значение obj;
         self.__prev = obj
 
     def get_next(self) -> 'ObjList':
@@ -66,9 +66,15 @@ class ObjList:
     def get_data(self) -> str:
         return self.__data
 
+    def __repr__(self) -> str:
+        return f'{self.__data}'
+
 
 lst = LinkedList()
 lst.add_obj(ObjList("данные 1"))
 lst.add_obj(ObjList("данные 2"))
 lst.add_obj(ObjList("данные 3"))
+lst.add_obj(ObjList("данные 4"))
+lst.remove_obj()
 res = lst.get_data()    # ['данные 1', 'данные 2', 'данные 3']
+print(res)
