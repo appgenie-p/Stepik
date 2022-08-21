962bd1f8dca28852fd43c51ded9c4fc1b08a9e1b
gennadiy-p <gennadiy-p@users.noreply.github.com>
2022-08-21 21:58:54 +0300

    111

--- 2.2.5 property.py
+++ 2.2.5 property.py
@@ -2,5 +2,30 @@ class WindowDlg:
     """
     wnd = WindowDlg(заголовок окна, ширина, высота)
     """
-    def __init__(self) -> None:
-        pass
+    def __init__(self, title: str, width: int, height: int) -> None:
+        self.__title: str = title
+        self.__width: int = width
+        self.__height: int = height
+
+    def show(self):
+        return f'{self.__title}: {self.width}, {self.height}'
+
+    @property
+    def width(self) -> int:
+        return self.__width
+
+    @width.setter
+    def width(self, width: int) -> None:
+        if isinstance(width, int) and 0 <= width <= 10000:
+            self.__width = width
+            self.show()
+
+    @property
+    def height(self) -> int:
+        return self.__height
+
+    @height.setter
+    def height(self, height: int) -> None:
+        if isinstance(height, int) and 0 <= height <= 10000:
+            self.__height = height
+            self.show()
---
+++ 2.2.6 binded list.py
@@ -0,0 +1,106 @@
+from typing import Optional, List
+
+
+class StackObj:
+    def __init__(self, data: str) -> None:
+        self.__data: Optional[str] = None
+        self.__next: Optional[StackObj] = None
+        self.data = data
+
+    @property
+    def data(self) -> str:
+        return self.__data
+
+    @data.setter
+    def data(self, data: str) -> None:
+        self.__data = data
+    
+    @property
+    def next(self) -> Optional['StackObj']:
+        return self.__next
+
+    @next.setter
+    def next(self, next_obj: Optional['StackObj']) -> None:
+        if isinstance(next_obj, StackObj) or next_obj is None:
+            self.__next = next_obj
+
+    def __repr__(self) -> str:
+        return f'{self.data}'
+    
+
+class Stack:
+    def __init__(self) -> None:
+        self.top: Optional[StackObj] = None
+
+    def push(self, obj: StackObj) -> None:
+        try:
+            last_obj = self.top
+            next_obj = self.top.next
+            while next_obj is not None:
+                last_obj = next_obj
+                next_obj = next_obj.next
+            last_obj.next = obj
+        except AttributeError:
+            self.top = obj
+
+    def pop(self) -> Optional[StackObj]:
+        if self.top is None:
+            return None
+        if not self.top.next:
+            deleted_obj = self.top
+            self.top = None
+            return deleted_obj
+        iter_obj = self.top
+        while iter_obj.next.next is not None:
+            iter_obj = iter_obj.next
+        iter_obj.next = None
+
+    def get_data(self) -> Optional[List[StackObj]]:
+        if not self.top:
+            return []
+        obj = self.top
+        objs_list =[]
+        while obj is not None:
+            objs_list.append(obj.data)
+            obj = obj.next
+        return objs_list
+
+    def __repr__(self) -> str:
+        return ' -> '.join(self.get_data())
+
+
+s = Stack()
+top = StackObj("obj_1")
+s.push(top)
+s.push(StackObj("obj_2"))
+s.push(StackObj("obj_3"))
+s.pop()
+
+res = s.get_data()
+assert res == ["obj_1", "obj_2"], f"метод get_data вернул неверные данные: {res}"
+assert s.top == top, "атрибут top объекта класса Stack содержит неверное значение"
+
+h = s.top
+while h:
+    res = h.data
+    h = h.next
+
+s = Stack()
+top = StackObj("obj_1")
+s.push(top)
+s.pop()
+assert s.get_data() == [], f"метод get_data вернул неверные данные: {s.get_data()}"
+
+n = 0
+h = s.top
+while h:
+    h = h.next
+    n += 1
+    
+assert n == 0, "при удалении всех объектов, стек-подобная стурктура оказалась не пустой"
+
+s = Stack()
+top = StackObj("name_1")
+s.push(top)
+obj = s.pop()
+assert obj == top, "метод pop() должен возвращать удаляемый объект"
