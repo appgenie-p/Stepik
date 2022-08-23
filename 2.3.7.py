cde1bf47eea6b8a1c30bcc6c5f26ed236befa3f1
Gennadiy Polezhaev <gpolejaev@gmail.com>
2022-08-23 22:46:09 +0300

    2.3.7

--- 2.3.7.py
+++ 2.3.7.py
@@ -1,46 +1,54 @@
+class Descriptor:
+    def __set_name__(self, obj, name) -> None:
+        self.name = '_' + name
+
+    def __set__(self, obj, value: str) -> None:
+        if self.checker(value):
+            setattr(obj, self.name, value)
+
+    def __get__(self, obj, type=None):
+        return getattr(obj, self.name)
+
+
+class StringValue(Descriptor):
+    def __init__(self, min_length: int = 2, max_length: int = 50) -> None:
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def checker(self, value: str):
+        return (isinstance(value, str)
+                and self.min_length <= len(value) <= self.max_length)
+
+
+class PriceValue(Descriptor):
+    def __init__(self, max_value: int = 10000) -> None:
+        self.max_value = max_value
+
+    def checker(self, value: str):
+        return (isinstance(value, int) or isinstance(value, float)
+                and value in range(0, self.max_value + 1))
+
+
+class Product:
+    name = StringValue()
+    price = PriceValue()
+
+    def __init__(self, good_name, good_price) -> None:
+        self.name = good_name
+        self.price = good_price
+
+
 class SuperShop:
-    """
-    myshop = SuperShop(название магазина)
-    """
     def __init__(self, name: str) -> None:
         self.name = name
-        self.goods: list = []
+        self.goods: list[Product] = []
 
     def add_product(self, product: 'Product') -> None:
         self.goods.append(product)
 
     def remove_product(self, product: 'Product') -> None:
-        self.goods.remove(product)
-
-
-class StringValue:
-    """
-    должен проверять, что присваивается строковый тип с длиной строки в
-    диапазоне [2; 50], т.е. min_length = 2, max_length = 50. Если проверки не
-    проходят, то соответствующие (прежние) значения меняться не должны.
-    """
-    pass
-
-
-class PriceValue :
-    """
-    должен проверять, что присваивается вещественное или целочисленное значение
-    в диапазоне [0; 10000], т.е. max_value = 10000. Если проверки не проходят,
-    то соответствующие (прежние) значения меняться не должны.
-    """
-    pass
-
-
-class Product:
-    """
-    pr = Product(наименование, цена)
-    """
-    name = StringValue(min_length=2, max_length=50)
-    price = PriceValue(max_value=10000)
-
-    def __init__(self, good_name, good_price) -> None:
-        self.name = good_name
-        self.price = good_price
+        if product in self.goods:
+            self.goods.remove(product)
 
 
 shop = SuperShop("У Балакирева")
@@ -48,3 +56,25 @@ shop.add_product(Product("Курс по Python", 0))
 shop.add_product(Product("Курс по Python ООП", 2000))
 for p in shop.goods:
     print(f"{p.name}: {p.price}")
+
+shop = SuperShop("У Балакирева")
+shop.add_product(Product("name", 100))
+shop.add_product(Product("name", 100))
+assert shop.name == "У Балакирева", "атрибут name объекта класса SuperShop содержит некорректное значение"
+
+for p in shop.goods:
+    assert p.price == 100, "дескриптор price вернул неверное значение"
+    assert p.name == "name", "дескриптор name вернул неверное значение"
+
+t = Product("name 123", 1000)
+shop.add_product(t)
+shop.remove_product(t)
+assert len(shop.goods) == 2, "неверное количество товаров: возможно некорректно работают методы add_product и remove_product"
+
+assert hasattr(shop.goods[0], 'name') and hasattr(shop.goods[0], 'price')
+
+t = Product(1000, "name 123")
+if hasattr(t, '_name'):
+    assert type(t.name) == str, "типы поля name должнен быть str"
+if hasattr(t, '_price'):
+    assert type(t.price) in (int, float), "тип поля price должнен быть int или float"
