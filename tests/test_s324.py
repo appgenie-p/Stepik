f7cd95a2cd1d8a47888065d5fc22f1bc6c040abd
appgenie <16236565+appgenie-p@users.noreply.github.com>
2022-08-26 12:47:29 +0000

    324

---
+++ tests/test_s324.py
@@ -0,0 +1,26 @@
+import pytest
+
+from s324 import LengthValidator, CharsValidator
+
+from string import ascii_lowercase, digits
+
+
+@pytest.fixture
+def length_validator():
+    return LengthValidator(3, 50)
+
+
+@pytest.fixture
+def chars_validator():
+    return CharsValidator(ascii_lowercase + digits)
+
+@pytest.mark.parametrize('val', [
+    ('zhopa', True, True),
+    ('zhopa1234', True, True),
+    ('zh', False, True),
+    ('zhopa@', True, False),
+])
+def test_length_validator_class(length_validator, chars_validator, val):
+    value, len_res, char_res,  = val
+    assert length_validator(value) is len_res
+    assert chars_validator(value) is char_res
