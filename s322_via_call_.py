5ac05919051921b1dd34bf26159253445995b816
appgenie <16236565+appgenie-p@users.noreply.github.com>
2022-08-26 08:44:50 +0000

    322-323

---
+++ s322_via_call_.py
@@ -0,0 +1,24 @@
+from typing import Any
+
+from random import randint, choices
+
+
+class RandomPassword:
+    def __init__(self, psw_chars: str, min_length: int, max_length: int):
+        self.psw_chars = psw_chars
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        pswd_len = randint(self.min_length, self.max_length)
+        return ''.join(choices(self.psw_chars, k=pswd_len))
+
+
+min_length = 5
+max_length = 20
+psw_chars = "qwertyuiopasdfghjklzxcvbnm0123456789!@#$%&*"
+
+rnd = RandomPassword(psw_chars, min_length, max_length)
+
+lst_pass = [rnd() for _ in range(3)]
+print(lst_pass)
---
+++ s323.py
@@ -0,0 +1,21 @@
+import typing
+
+from typing import Any
+
+
+class ImageFileAcceptor:
+    def __init__(self, extensions: tuple) -> None:
+        self.extensions = extensions
+
+    def __call__(self, file_name) -> Any:
+        return file_name.split('.')[-1] in self.extensions
+
+
+fs = ["boat.jpg", "web.png", "text.txt", "python.doc", "ava.8.jpg", "forest.jpeg", "eq_1.png", "eq_2.png", "my.html", "data.shtml"]
+acceptor = ImageFileAcceptor(("jpg", "png"))
+res = filter(acceptor, fs)
+assert set(res) == set(["boat.jpg", "web.png", "ava.8.jpg", "eq_1.png", "eq_2.png"]), "с помощью объекта класса ImageFileAcceptor был сформирован неверный список файлов"
+
+acceptor = ImageFileAcceptor(("jpeg", "html"))
+res = filter(acceptor, fs)
+assert set(res) == set(["forest.jpeg", "my.html"]), "с помощью объекта класса ImageFileAcceptor был сформирован неверный список файлов"
