1c4267221f5ea49c379e21603dbc0de5f7e5e49e
appgenie <16236565+appgenie-p@users.noreply.github.com>
2022-10-18 14:02:59 +0000

    update

--- s366.py
+++ s366.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Union
 
 
@@ -13,4 +14,54 @@ class ShopItem:
         return hash(self) == hash(comparable)
 
     def __hash__(self) -> int:
-        return hash(self.name) + hash(self.weight) + hash(self.price)
+        return hash((self.name.lower(), self.weight, self.price))
+
+    def __repr__(self):
+        return f'Object: {self.name}'
+
+
+lst_in = list(map(str.strip, sys.stdin.readlines()))
+
+# lst_in = [
+#     "Системный блок: 1500 75890.56",
+#     "Монитор Samsung: 2000 34000",
+#     "Клавиатура: 200.44 545",
+#     "Монитор Samsung: 2000 34000"
+# ]
+
+shop_items: dict = {}
+
+for string in lst_in:
+    string_dev = string.split(":")
+    name = string_dev[0]
+    weight, price = string_dev[1].strip().split()
+    item = ShopItem(name, float(weight), float(price))
+    if item in shop_items:
+        shop_items[item][1] += 1
+    else:
+        shop_items[item] = [item, 1]
+
+
+it1 = ShopItem('name', 10, 11)
+it2 = ShopItem('name', 10, 11)
+assert hash(it1) == hash(it2), "разные хеши у равных объектов"
+
+it2 = ShopItem('name', 10, 12)
+assert hash(it1) != hash(it2), "равные хеши у разных объектов"
+
+it2 = ShopItem('name', 11, 11)
+assert hash(it1) != hash(it2), "равные хеши у разных объектов"
+
+it2 = ShopItem('NAME', 10, 11)
+assert hash(it1) == hash(it2), "разные хеши у равных объектов"
+
+name = lst_in[0].split(':')
+for sp in shop_items.values():
+    assert isinstance(sp[0], ShopItem) and type(sp[1]) == int, "в значениях словаря shop_items первый элемент должен быть объектом класса ShopItem, а второй - целым числом"
+
+v = list(shop_items.values())
+if v[0][0].name.strip() == "Системный блок":
+    assert v[0][1] == 1 and v[1][1] == 2 and v[2][1] == 1 and len(v) == 3, "неверные значения в словаре shop_items"
+
+if v[0][0].name.strip() == "X-box":
+    assert v[0][1] == 2 and v[1][1] == 1 and v[2][1] == 2 and len(v) == 3, "неверные значения в словаре shop_items"
--- temp.py
+++ temp.py
@@ -1,4 +1,58 @@
-import sys
+from typing import Union
 
-print(f"Name of the script      : {sys.argv[0]=}")
-print(f"Arguments of the script : {sys.argv[1:]=}")
+class ShopItem:
+    Number = Union[int, float]
+
+    def __init__(self, name: str, weight: Number, price: Number) -> None:
+        self.name = name
+        self.weight = weight
+        self.price = price
+
+    def __eq__(self, comparable) -> bool:
+        return hash(self) == hash(comparable)
+
+    def __hash__(self) -> int:
+        return hash((self.name.lower(), self.weight, self.price))
+
+    def __repr__(self):
+        return f'Object: {self.name}'
+
+
+lst_in = [
+    "Системный блок: 1500 75890.56",
+    "Монитор Samsung: 2000 34000",
+    "Клавиатура: 200.44 545",
+    "Монитор Samsung: 2000 34000"
+]
+
+shop_items: dict = {}
+
+for string in lst_in:
+    name, data = string.split(": ")
+    item = ShopItem(name, *map(float, data.split()))
+    shop_items.setdefault(item, [item, 0])[1] += 1
+
+
+it1 = ShopItem('name', 10, 11)
+it2 = ShopItem('name', 10, 11)
+assert hash(it1) == hash(it2), "разные хеши у равных объектов"
+
+it2 = ShopItem('name', 10, 12)
+assert hash(it1) != hash(it2), "равные хеши у разных объектов"
+
+it2 = ShopItem('name', 11, 11)
+assert hash(it1) != hash(it2), "равные хеши у разных объектов"
+
+it2 = ShopItem('NAME', 10, 11)
+assert hash(it1) == hash(it2), "разные хеши у равных объектов"
+
+name = lst_in[0].split(':')
+for sp in shop_items.values():
+    assert isinstance(sp[0], ShopItem) and type(sp[1]) == int, "в значениях словаря shop_items первый элемент должен быть объектом класса ShopItem, а второй - целым числом"
+
+v = list(shop_items.values())
+if v[0][0].name.strip() == "Системный блок":
+    assert v[0][1] == 1 and v[1][1] == 2 and v[2][1] == 1 and len(v) == 3, "неверные значения в словаре shop_items"
+
+if v[0][0].name.strip() == "X-box":
+    assert v[0][1] == 2 and v[1][1] == 1 and v[2][1] == 2 and len(v) == 3, "неверные значения в словаре shop_items"
