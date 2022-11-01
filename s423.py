cc08905fcc3d96c64bc867556aee1008155f39cd
Gennadiy Polezhaev <gpolejaev@gmail.com>
2022-11-01 21:19:39 +0300

    s423

--- s423.py
+++ s423.py
@@ -1,16 +1,18 @@
 class ListInteger(list):
-    def __init__(self, lst):
-        if not isinstance(lst, (tuple, list)):
-            raise TypeError('можно передавать только кортеж или список')
-        self.lst = list(lst)
+    def __init__(self, lst: list):
+        for val in lst:
+            self.__check_val(val)
+        super().__init__(lst)
 
-    def __setitem__(self, index: int, value: int) -> None:
-        self.lst[index] = value
+    @staticmethod
+    def __check_val(val):
+        if type(val) != int:
+            raise TypeError('можно передавать только целочисленные значения')
 
+    def __setitem__(self, index, value):
+        self.__check_val(value)
+        super().__setitem__(index, value)
 
-s = ListInteger([1, 2, 3])
-s[1] = 10
-print(s)
-s.append(11)
-print(s)
-# s[0] = 10.5 # TypeError
+    def append(self, value):
+        self.__check_val(value)
+        super().append(value)
