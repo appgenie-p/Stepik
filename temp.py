1af0d4b500edd2551442b4e8c368afae2b6fad0f
gennadiy-p <gennadiy-p@users.noreply.github.com>
2022-11-22 17:52:39 +0300

    minor

--- temp.py
+++ temp.py
@@ -1,27 +1,19 @@
-class Auto:
-    __MIN_WEIGHT = 100
-    __MAX_WEIGHT = 1000
-
-    def __init__(self, model):
-        self.__verify_model(model)
-        self.__model = model
-
-    def __verify_model(self, model):
-        if type(model) != str:
-            raise TypeError('модель должна представляться строкой')
+class A:
+    def __init__(self, name, old):
+        super().__init__()
+        self.name = name
+        self.old = old
 
 
-class BMW(Auto):
-    def __init__(self, model, weight):
-        super().__init__(model)
-        self.__verify_weight(weight)
-        self.__weight = weight
-
-    def __verify_weight(self, weight):
-        if self.__MIN_WEIGHT > weight or weight > self.__MAX_WEIGHT:
-            raise TypeError(f'вес автомобиля BMW должен быть в пределах [{self.__MIN_WEIGHT}; {self.__MAX_WEIGHT}]')
+class B:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
-bmw_x5 = BMW('BMW X5', 512.5)
-print(bmw_x5._BMW__weight)
-print(bmw_x5._Auto__model)
+class C(B, A):
+    def __init__(self, name, old, weight, height):
+        super().__init__(name, old)
+        self.weight = weight
+        self.height = height
+
+person = C("Balakirev", 33, 80, 185)
