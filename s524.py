91530c1d39d48fe6730da541391a2f7b5f8169be
Gennadiy Polezhaev <gpolejaev@gmail.com>
2022-12-02 21:45:09 +0300

    s524

--- s524.py
+++ s524.py
@@ -1,7 +1,10 @@
-# first, second = input().split()
-from typing import Union
-
-
-first, second = '8.0', '11.1'
-
-
+first, second = input().split()
+try:
+    first, second = [int(val) for val in (first, second)]
+except Exception:
+    try:
+        first, second = [float(val) for val in (first, second)]
+    except Exception:
+        pass
+finally:
+    print(first + second)
