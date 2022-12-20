76f9df62dc134f9b5085120f430832f711f8201f
Gennadiy Polezhaev <gpolejaev@gmail.com>
2022-12-20 22:28:37 +0300

    534 started

---
+++ s534.py
@@ -0,0 +1,17 @@
+class ValidatorString:
+    def __init__(self, min_length: int, max_length: int, chars: str):
+        # Если chars пустая строка,  проверку на вхождение символов не делать.
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def is_valid(self, string: str) -> bool:
+        pass
+
+
+class LoginForm:
+    def __init__(self, login_validator: ValidatorString,
+                 password_validator: ValidatorString) -> None:
+        self.login_validator = login_validator
+        self.password_validator = password_validator
+
+    
