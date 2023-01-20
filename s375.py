32a23d857eaba49bdcb6dd6fe79a62d4e66f3342
Gennady Polezhaev <gpolejaev@gmail.com>
2023-01-20 10:17:12 +0300

    final revision

--- s375.py
+++ s375.py
@@ -2,7 +2,6 @@ import sys
 
 
 class MailItem:
-    # item = MailItem(mail_from, title, content)
     def __init__(self, mail_from: str, title: str, content: str) -> None:
         self.mail_from, self.title, self.content = mail_from, title, content
         self.is_read = False
@@ -16,32 +15,28 @@ class MailItem:
 
 class MailBox:
     def __init__(self) -> None:
-        # список из принятых писем
         self.inbox_list: list = []
 
     def receive(self) -> None:
-        lst_in = list(map(str.strip, sys.stdin.readlines()))
-        # lst_in = [
-        #     'sc_lib@list.ru; От Балакирева; Успехов в IT!',
-        #     'mail@list.ru; Выгодное предложение; Вам одобрен кредит.',
-        #     'Python ООП; Балакирев С.М.; 2022',
-        #     'mail123@list.ru; Розыгрыш; Вы выиграли 1 млн. руб. Переведите 30 тыс. руб., чтобы его получить.'
-        # ]
-        # iterrate over list ->
+        # lst_in = list(map(str.strip, sys.stdin.readlines()))
+        lst_in = [
+            'sc_lib@list.ru; От Балакирева; Успехов в IT!',
+            'mail@list.ru; Выгодное предложение; Вам одобрен кредит.',
+            'Python ООП; Балакирев С.М.; 2022',
+            'mail123@list.ru; Розыгрыш; Вы выиграли 1 млн. руб. Переведите 30 тыс. руб., чтобы его получить.'
+        ]
         for item in lst_in:
-            # each item separate on 3 parts "от кого; заголовок; текст письма" ->
-            parts = item.split(';')
-            # and create MailItem object ->
-            obj = MailItem(*parts)
-            # append this object to self.inbox_list
-            self.inbox_list.append(obj)
-            
+            self.inbox_list.append(MailItem(*item.split(';')))
+
 
 mail = MailBox()
 mail.receive()
-mail.inbox_list[0].set_read(True)
-mail.inbox_list[-1].set_read(True)
-# Сформируйте в программе список (глобальный) с именем inbox_list_filtered.
-# В этот список поместите только те письма, которые прочитаны.
-# Используя стандартную функцию filter() совместно с функцией bool() языка Python.
+
+# Get first and last elements of mail.inbox_list and set them as read
+# by using list slicing. Slise first and last elements of list with step equal to
+# list length minus 1 (4-1=3 in the case). Then set them as read.
+for i in mail.inbox_list[::len(mail.inbox_list) - 1]:
+    i.set_read(True)
+
 inbox_list_filtered = list(filter(bool, mail.inbox_list))
+
