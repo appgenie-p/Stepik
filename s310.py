26379dd4a4e9928c07bdabd3efb82e0993936a2c
Gennadiy Polezhaev <gpolejaev@gmail.com>
2023-06-06 17:51:07 +0300

    asss

--- s310.py
+++ s310.py
@@ -52,5 +52,25 @@ class TicTacToe:
         cell.value = value
 
 
-ss = TicTacToe()
-ss.show()
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.init()
+    step_game = 0
+    while game:
+        game.show()
+
+        if step_game % 2 == 0:
+            game.human_go()
+        else:
+            game.computer_go()
+
+        step_game += 1
+
+    game.show()
+
+    if game.is_human_win:
+        print("Поздравляем! Вы победили!")
+    elif game.is_computer_win:
+        print("Все получится, со временем")
+    else:
+        print("Ничья.")
