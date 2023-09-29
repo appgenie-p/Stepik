8051f9338766aa280c3c60e4b9c0f0949ba82396
Gennadiy Polezhaev <gpolejaev@gmail.com>
2023-09-29 13:32:29 +0300

    528 done

--- s528.py
+++ s528.py
@@ -12,12 +12,9 @@ class Rect:
         self._height = height
 
     def __setattr__(self, key: Any, value: Any):
-        if type(value) not in (int, float):
-            raise ValueError(
-                "некорректные координаты и параметры прямоугольника"
-            )
+        _raise_err_if_value_is_not_int_or_float(value)
         if key in ("_width", "_height"):
-            _check_negative_value(value)
+            _raise_err_if_negative_value(value)
         super().__setattr__(key, value)
 
     def __repr__(self):
@@ -36,85 +33,44 @@ class Rect:
         return False
 
 
-def _check_negative_value(value: Number) -> None:
+def _raise_err_if_value_is_not_int_or_float(value: Any) -> None:
+    if type(value) not in (int, float):
+        raise ValueError("некорректные координаты и параметры прямоугольника")
+
+
+def _raise_err_if_negative_value(value: Number) -> None:
     if value <= 0:
         raise ValueError("некорректные координаты и параметры прямоугольника")
 
 
-def _get_uncrossed_rectangles(combinations: List[Tuple[Rect, Rect]]) -> List[Rect]:
-    lst_not_collision: Set[Rect] = set()
+def _get_crossed_rectangles(
+    combinations: List[Tuple[Rect, Rect]]
+) -> Set[Rect]:
     failed_members: Set[Rect] = set()
     for rect1, rect2 in combinations:
         try:
             rect1.is_collision(rect2)
         except TypeError:
-            failed_members.add(rect1)
-        else:
-            lst_not_collision.add(rect1)
-    lst_not_collision.difference_update(failed_members)
-    return list(lst_not_collision)
+            failed_members.update((rect1, rect2))
+    return failed_members
 
-
-cords_raw = """
-0; 0; 5; 3
-6; 0; 3; 5
-3; 2; 4; 4
-0; 8; 8; 1
-"""
-
-data_for_rects = [line.split("; ") for line in cords_raw.strip().splitlines()]
-
-lst_rect = [Rect(*map(eval, cords)) for cords in data_for_rects]
-
-all_combinations = list(itertools.combinations(lst_rect, 2))
-
-lst_not_collision = _get_uncrossed_rectangles(all_combinations)
-pass
 
 if __name__ == "__main__":
-    r = Rect(1, 2, 10, 20)
-    assert (
-        r._x == 1 and r._y == 2 and r._width == 10 and r._height == 20
-    ), "неверные значения атрибутов объекта класса Rect"
+    cords_raw = """
+    0; 0; 5; 3
+    6; 0; 3; 5
+    3; 2; 4; 4
+    0; 8; 8; 1
+    """
 
-    r2 = Rect(1.0, 2, 10.5, 20)
+    data_for_rects = [
+        line.split("; ") for line in cords_raw.strip().splitlines()
+    ]
 
-    try:
-        r2 = Rect(0, 2, 0, 20)
-    except ValueError:
-        assert True
-    else:
-        assert (
-            False
-        ), "не сгенерировалось исключение ValueError при создании объекта Rect(0, 2, 0, 20)"
+    lst_rect = [Rect(*map(eval, cords)) for cords in data_for_rects]
 
-    assert len(data_for_rects) == 4, "список lst_rect содержит не 4 элемента"
-    assert (
-        len(lst_not_collision) == 1
-    ), "неверное число элементов в списке lst_not_collision"
+    all_combinations = list(itertools.combinations(lst_rect, 2))
 
-    def not_collision(rect):
-        for x in data_for_rects:
-            try:
-                if x != rect:
-                    rect.is_collision(x)
-            except TypeError:
-                return False
-        return True
-
-    f = list(filter(not_collision, lst_rect))
-    assert (
-        lst_not_collision == f
-    ), "неверно выделены не пересекающиеся прямоугольники, возможно, некорректно работает метод is_collision"
-
-    r = Rect(3, 2, 2, 5)
-    rr = Rect(1, 4, 6, 2)
-
-    try:
-        r.is_collision(rr)
-    except TypeError:
-        assert True
-    else:
-        assert (
-            False
-        ), "не сгенерировалось исключение TypeError при вызове метода is_collision() для прямоугольников Rect(3, 2, 2, 5) и Rect(1, 4, 6, 2)"
+    lst_not_collision = list(
+        set(lst_rect) - _get_crossed_rectangles(all_combinations)
+    )
