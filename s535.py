4ffb3a98203e303a048f272935be26385932211d
Gennadiy Polezhaev <gpolejaev@gmail.com>
2023-10-02 13:24:25 +0300

    535

--- s535.py
+++ s535.py
@@ -12,22 +12,52 @@ class Test:
 
 def _check_descr_size(descr: str):
     if not 10 <= len(descr) <= 10000:
-        raise ValueError("Description must be 10-10000 characters long")
+        raise ValueError(
+            "формулировка теста должна быть от 10 до 10 000 символов"
+        )
 
 
 Number = Union[int, float]
 
 
 class TestAnsDigit(Test):
-    def __init__(self, descr: str, ans_digit: Number, max_error_digit: Number):
+    def __init__(
+        self, descr: str, ans_digit: Number, max_error_digit: Number = 0.01
+    ):
         super().__init__(descr)
         self.ans_digit = ans_digit
         self.max_error_digit = max_error_digit
 
     def __setattr__(self, key: Any, value: Any) -> None:
-        if key in ("ans_digit", "max_error_digit") and type(value) not in (
-            int, float
+        if (
+            key in ("ans_digit", "max_error_digit")
+            and type(value)
+            not in (
+                int,
+                float,
+            )
+            or (key == "max_error_digit" and value < 0)
         ):
             raise ValueError("недопустимые значения аргументов теста")
-        if key == "max_error_digit" and value < 0:
-            raise ValueError("недопустимые значения аргументов теста")
+        super().__setattr__(key, value)
+
+    def run(self) -> bool:
+        ans = float(input())
+        ans_digit = self.ans_digit
+        max_error_digit = self.max_error_digit
+        return (
+            ans_digit - max_error_digit <= ans <= ans_digit + max_error_digit
+        )
+
+
+def main() -> Union[Exception, bool]:
+    descr, ans = map(str.strip, input().split("|"))
+    try:
+        test = TestAnsDigit(descr, float(ans))
+        return test.run()
+    except Exception as e:
+        return e
+
+
+if __name__ == "__main__":
+    print(main())
--- s535_tests.py
+++ s535_tests.py
@@ -1,10 +1,7 @@
 import pytest
+from _pytest.monkeypatch import MonkeyPatch
 
-from s535 import Test, TestAnsDigit
-
-
-def test_test_class_creation():
-    assert Test("test")
+from s535 import Test, TestAnsDigit, main
 
 
 def test_test_creation_err_with_wrong_descr_size():
@@ -25,7 +22,10 @@ def test_test_run_method_definition_in_child_class():
 
 
 def test_Test_Ans_Digit_class():
-    assert TestAnsDigit("testtesttest", 1, 1)
+    test = TestAnsDigit("testtesttest", 1, 1)
+    assert test.name == "testtesttest"
+    assert test.ans_digit == 1
+    assert test.max_error_digit == 1
 
 
 def test_Test_Ans_Digit_class_err1():
@@ -36,3 +36,73 @@ def test_Test_Ans_Digit_class_err1():
 def test_Test_Ans_Digit_class_err():
     with pytest.raises(ValueError):
         TestAnsDigit("testtesttest", "1", 1)
+    with pytest.raises(ValueError):
+        TestAnsDigit("testtesttest", True, 1)
+
+
+@pytest.fixture
+def user_input(monkeypatch: MonkeyPatch):
+    monkeypatch.setattr("builtins.input", lambda: "5.05")
+
+
+def test_testansdigit_run_valid_answer(monkeypatch: MonkeyPatch):
+    test = TestAnsDigit("Test description", 5.0, 0.1)
+    monkeypatch.setattr("builtins.input", lambda: "5.05")
+    assert test.run() == True
+
+
+def test_testansdigit_run_envalid_answer(monkeypatch: MonkeyPatch):
+    test = TestAnsDigit("Test description", 5.0, 0.1)
+    monkeypatch.setattr("builtins.input", lambda: "5.2")
+    assert test.run() == False
+
+
+def test_1():
+    try:
+        test = Test("descr")
+    except ValueError:
+        assert True
+    else:
+        assert False, (
+            "не сгенерировалось исключение ValueError при вызове "
+            "инициализатора класса Test с неверным набором аргументов"
+        )
+
+
+def test_2():
+    try:
+        test = Test("descr ghgfhgjg ghjghjg")
+        test.run()
+    except NotImplementedError:
+        assert True
+    else:
+        assert False
+
+    assert issubclass(TestAnsDigit, Test)
+
+    t = TestAnsDigit("ffhgfh fghfghfghfggfhfghfh", 1)
+    t = TestAnsDigit("ffhgfh fghfghfghfggfhfghfh", 1, 0.5)
+
+    try:
+        t = TestAnsDigit("ffhgfh fghfghfghfggfhfghfh", 1, -0.5)
+    except ValueError:
+        assert True
+    else:
+        assert False
+
+
+def test_main_valid(monkeypatch: MonkeyPatch):
+    input_1 = "Какое значение получим, при выполнении команды int(5.7)? | 5"
+    input_2 = "2"
+    inputs = iter([input_1, input_2])
+    monkeypatch.setattr("builtins.input", lambda: next(inputs))
+    assert main() is False
+
+
+def test_main_invalid(monkeypatch: MonkeyPatch):
+    input_1 = "Как | 5"
+    input_2 = "2"
+    inputs = iter([input_1, input_2])
+    monkeypatch.setattr("builtins.input", lambda: next(inputs))
+    with pytest.raises(ValueError):
+        main()
