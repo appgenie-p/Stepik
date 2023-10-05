75c35fb54ee2505d099404ed8a8dfc89b240de7c
Gennadiy Polezhaev <gpolejaev@gmail.com>
2023-10-05 15:20:04 +0300

    560 DONE

--- s560.py
+++ s560.py
@@ -1,5 +1,6 @@
+import random
 from enum import Enum
-from typing import List, NamedTuple, Optional, Set
+from typing import List, NamedTuple, Optional, Set, Tuple, cast
 
 
 class Direction(int, Enum):
@@ -35,60 +36,31 @@ class Ship:
     def __setitem__(self, key: int, value: int) -> None:
         if value not in (1, 2):
             raise ValueError("value must be 1 or 2")
-        self._cells[key] = Hit.intact if value == 1 else Hit.hit
+        self._cells[key] = Hit(value)
 
     def __getitem__(self, key: int) -> int:
         return self._cells[key]
 
     @property
-    def around(self) -> Set[Coords]:
-        if self.tp == "horizontal":
-            return self._around_horizontal_ship()
-        return self._around_vertical_ship()
-
-    def _around_vertical_ship(self) -> Set[Coords]:
-        above = {Coords(self.coords_init.x, self.coords_init.y - 1)}
-        below = {Coords(self.coords_init.x, self.coords_init.y + self.length)}
-        left = {
-            Coords(self.coords_init.x - 1, self.coords_init.y + y)
-            for y in range(-1, self.length + 1)
-        }
-        right = {
-            Coords(self.coords_init.x + 1, self.coords_init.y + y)
-            for y in range(-1, self.length + 1)
-        }
-        return above.union(below, left, right)
-
-    def _around_horizontal_ship(self) -> Set[Coords]:
-        above = {
-            Coords(self.coords_init.x + x, self.coords_init.y - 1)
-            for x in range(-1, self.length + 1)
-        }
-        below = {
-            Coords(self.coords_init.x + x, self.coords_init.y + 1)
-            for x in range(-1, self.length + 1)
-        }
-        left = {Coords(self.coords_init.x - 1, self.coords_init.y)}
-        right = {Coords(self.coords_init.x + self.length, self.coords_init.y)}
-        return above.union(below, left, right)
-
-    @property
-    def coords(self) -> Set[Coords]:
-        if self.tp == "horizontal":
-            return {
-                Coords(self.coords_init.x + x, self.coords_init.y)
-                for x in range(self.length)
-            }
+    def cells_around(self) -> Set[Coords]:
+        """Get set of cell's coordinates around ship"""
         return {
-            Coords(self.coords_init.x, self.coords_init.y + y)
-            for y in range(self.length)
-        }
+            Coords(coord.x + i, coord.y + j)
+            for coord in self.cells
+            for i in range(-1, 2)
+            for j in range(-1, 2)
+            if not (i == 0 and j == 0)
+        } - self.cells
 
     @property
-    def coords_init(self) -> Coords:
-        if self._x is None or self._y is None:
-            raise ValueError("Ship is not initialized")
-        return Coords(self._x, self._y)
+    def cells(self) -> Set[Coords]:
+        """
+        Returns a set of coordinates representing the cells occupied by the
+        ship.
+        """
+        if self.direction.name == "horizontal":
+            return {Coords(self.x + x, self.y) for x in range(self._length)}
+        return {Coords(self.x, self.y + y) for y in range(self._length)}
 
     @property
     def x(self) -> int:
@@ -111,19 +83,21 @@ class Ship:
         self._y = y
 
     @property
-    def tp(self) -> str:
-        return Direction(self._tp).name
+    def direction(self) -> Direction:
+        return Direction(self._tp)
+
+    @direction.setter
+    def direction(self, tp: int) -> None:
+        if tp not in (1, 2):
+            raise ValueError("tp must be 1 or 2")
+        self._tp = tp
 
     @property
-    def length(self) -> int:
-        return self._length
-
-    @property
-    def cells(self) -> List[Hit]:
+    def cells_condition(self) -> List[Hit]:
         return self._cells
 
     @property
-    def is_move(self) -> bool:
+    def is_moveable(self) -> bool:
         return self._is_move
 
     def set_start_coords(self, x: int, y: int) -> None:
@@ -131,42 +105,102 @@ class Ship:
         self._y = y
 
     def get_start_coords(self) -> Coords:
-        return self.coords_init
+        return Coords(self.x, self.y)
 
     def move(self, go: int) -> None:
-        self.check_go_value(go)
-        self.check_ship_initialized()
+        self._check_go_value(go)
+        self._check_ship_initialized()
         self.move_ship(go)
 
-    def check_ship_initialized(self):
+    def _check_ship_initialized(self):
         if self._x is None or self._y is None:
             raise ValueError("Ship is not initialized")
 
-    def check_go_value(self, go: int):
+    def _check_go_value(self, go: int):
         if go not in (1, -1):
             raise ValueError("go must be 1 or -1")
 
     def move_ship(self, go: int) -> None:
-        if self.is_move:
-            if self.tp == "horizontal":
+        if self.is_moveable:
+            if self.direction.name == "horizontal":
                 self._x += go  # type: ignore
-            elif self.tp == "vertical":
+            elif self.direction.name == "vertical":
                 self._y += go  # type: ignore
 
     def is_collide(self, other: "Ship") -> bool:
-        return bool(self.around.intersection(other.coords))
+        """
+        Check if the current ship collides with another ship.
+        True if the ships collide, False otherwise.
+        """
+        return bool(self.cells_around.intersection(other.cells))
 
     def is_out_pole(self, size: int = 10) -> bool:
-        for coord in self.coords:
-            if (
-                coord.x < 0
-                or coord.y < 0
-                or coord.x >= size
-                or coord.y >= size
-            ):
-                return True
-        return False
+        return any(
+            coord.x < 0 or coord.y < 0 or coord.x >= size or coord.y >= size
+            for coord in self.cells
+        )
 
 
 class GamePole:
-    pass
+    def __init__(self, size: int = 10):
+        self._size = size
+        self._ships: List[Ship] = []
+
+    def init(self):
+        """
+        Initializes the class by creating a fleet of ships with decreasing deck
+        sizes from 4 to 1.
+        """
+        [
+            [self._set_ship(deck_size) for _ in range(ship_number)]
+            for ship_number, deck_size in enumerate(range(4, 0, -1), 1)
+        ]
+
+    def _set_ship(self, deck_size: int):
+        ship = Ship(deck_size)
+        while True:
+            ship.direction = cast(Direction, random.randint(1, 2))
+            ship.x = random.randint(0, self._size - 1)
+            ship.y = random.randint(0, self._size - 1)
+            if not ship.is_out_pole(self._size) and not any(
+                ship.is_collide(other) for other in self._ships
+            ):
+                break
+        self._ships.append(ship)
+        return ship
+
+    def get_ships(self):
+        return self._ships
+
+    def move_ships(self):
+        for ship in self._ships:
+            original_coords = ship.x, ship.y
+            first_try = random.choice((-1, 1))
+            if self._moved(ship, original_coords, first_try):
+                continue
+            second_try = -first_try
+            if self._moved(ship, original_coords, second_try):
+                continue
+            ship.x, ship.y = original_coords
+
+    def _moved(
+        self, ship: Ship, original_coords: Tuple[int, int], move_len: int
+    ) -> bool:
+        ship.move(move_len)
+        if ship.is_out_pole(self._size) or any(
+            ship.is_collide(other) for other in self._ships
+        ):
+            return False
+        return True
+
+    def get_pole(self) -> Tuple[Tuple[str, ...], ...]:
+        pole = [["0"] * self._size for _ in range(self._size)]
+        for ship in self._ships:
+            for coord, cell in zip(ship.cells, ship.cells_condition):
+                pole[coord.x][coord.y] = "2" if cell == Hit.hit else "1"
+        return tuple(tuple(row) for row in pole)
+
+    def show(self) -> None:
+        pole = self.get_pole()
+        for row in pole:
+            print(" ".join(row))
--- s560_tests.py
+++ s560_tests.py
@@ -17,25 +17,25 @@ def test_ship_creation() -> None:
 def test_ship_attrs():
     ship = Ship(3, 2, 0, 0)
     assert (
-        ship.length == 3
-        and ship.tp == "vertical"
-        and ship.coords_init.x == 0
-        and ship.coords_init.y == 0
+        ship._length == 3
+        and ship.direction.value == 2
+        and ship.x == 0
+        and ship.y == 0
     ), "неверные значения атрибутов объекта класса Ship"
-    assert ship.tp == "vertical", "неверное значение атрибута tp"
+    assert ship.direction.name == "vertical", "неверное значение атрибута tp"
 
 
 def test_ship_cells_and_move(ship4v: Ship):
     ship = ship4v
-    assert ship.cells == [1, 1, 1], "неверный список _cells"
-    assert ship.is_move, "неверное значение атрибута _is_move"
+    assert ship.cells_condition == [1, 1, 1], "неверный список _cells"
+    assert ship.is_moveable, "неверное значение атрибута _is_move"
 
 
 def test_ship_set_start_coords(ship4v: Ship):
     ship = ship4v
     ship.set_start_coords(1, 2)
     assert (
-        ship.coords_init.x == 1 and ship.coords_init.y == 2
+        ship.x == 1 and ship.y == 2
     ), "неверно отработал метод set_start_coords()"
 
 
@@ -50,7 +50,7 @@ def test_ship_get_start_coords(ship4v: Ship):
 def test_ship_move(ship4v: Ship):
     ship = Ship(3, 2, 0, 0)
     ship.move(1)
-    assert ship.coords_init == (0, 1)
+    assert ship.get_start_coords() == (0, 1)
 
 
 def test_ship3h_around():
@@ -59,7 +59,7 @@ def test_ship3h_around():
     below = {(0, 2), (1, 2), (2, 2), (3, 2), (4, 2)}
     left = {(0, 1)}
     right = {(4, 1)}
-    assert s1.around == above.union(below, left, right)
+    assert s1.cells_around == above.union(below, left, right)
 
 
 def test_ship2v_around():
@@ -68,7 +68,7 @@ def test_ship2v_around():
     below = {(0, 3), (1, 3), (2, 3)}
     left = {(0, 1), (0, 2)}
     right = {(2, 1), (2, 2)}
-    assert s1.around == above.union(below, left, right)
+    assert s1.cells_around == above.union(below, left, right)
 
 
 def test_ship1v_around():
@@ -77,7 +77,7 @@ def test_ship1v_around():
     below = {(-1, 1), (0, 1), (1, 1)}
     left = {(-1, 0)}
     right = {(1, 0)}
-    assert s1.around == above.union(below, left, right)
+    assert s1.cells_around == above.union(below, left, right)
 
 
 def test_ships():
@@ -124,28 +124,45 @@ def test_ship_cells_edit_by_index():
     assert s2[0] == 2, "неверно работает обращение ship[indx]"
 
 
-# p = GamePole(10)
-# p.init()
-# for nn in range(5):
-#     for s in p._ships:
-#         assert (
-#             s.is_out_pole(10) == False
-#         ), "корабли выходят за пределы игрового поля"
+def test_set_ship():
+    game_pole = GamePole(size=10)
+    ship = game_pole._set_ship(4)
+    assert ship.direction.name in ["horizontal", "vertical"]
+    assert 0 <= ship.x < 10
+    assert 0 <= ship.y < 10
+    assert len(ship.cells_condition) == 4
 
-#         for ship in p.get_ships():
-#             if s != ship:
-#                 assert (
-#                     s.is_collide(ship) == False
-#                 ), "корабли на игровом поле соприкасаются"
-#     p.move_ships()
 
-# gp = p.get_pole()
-# assert (
-#     type(gp) == tuple and type(gp[0]) == tuple
-# ), "метод get_pole должен возвращать двумерный кортеж"
-# assert (
-#     len(gp) == 10 and len(gp[0]) == 10
-# ), "неверные размеры игрового поля, которое вернул метод get_pole"
+def test_ships_init():
+    p = GamePole(10)
+    p.init()
+    p.show()
+    for _ in range(5):
+        for s in p._ships:
+            assert (
+                s.is_out_pole(10) is False
+            ), "корабли выходят за пределы игрового поля"
 
-# pole_size_8 = GamePole(8)
-# pole_size_8.init()
+            for ship in p.get_ships():
+                if s != ship:
+                    assert (
+                        s.is_collide(ship) is False
+                    ), "корабли на игровом поле соприкасаются"
+        p.move_ships()
+
+
+def test_get_pole():
+    p = GamePole(10)
+    p.init()
+    p.show()
+    gp = p.get_pole()
+    assert (
+        type(gp) == tuple and type(gp[0]) == tuple
+    ), "метод get_pole должен возвращать двумерный кортеж"
+    assert (
+        len(gp) == 10 and len(gp[0]) == 10
+    ), "неверные размеры игрового поля, которое вернул метод get_pole"
+
+
+pole_size_8 = GamePole(8)
+pole_size_8.init()
